4ebc157836f867f04aae1a21f2857f10ac9e9363
soracocin <soradecochinchine@gmail.com>
2025-11-10 19:37:47 +0700

    refactor

--- backend/src/main/java/com/cocin/waifuwar/service/FileStorageService.java
+++ backend/src/main/java/com/cocin/waifuwar/service/FileStorageService.java
@@ -7,7 +7,6 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.nio.file.*;
 import java.util.UUID;
 
